d0cf0f8d239530046a583ca4914d480de41d0994
Enes Goksu <enes-goksu.fb@hotmail.com>
2023-04-06 14:31:54 +0300

    Sessiz Harf tamamlandı

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SessizHarf.SessizHarf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir kelime veya cümle yazınız: ");
+            string ifade = Console.ReadLine();
+            string[] kelime = ifade.Split(' ');
+            string unsuz = "bcçdfgğhjklmnprsştvyz";
+
+            foreach (var harf in kelime)
+            {
+                bool kontrol = false;
+                for (int i = 1; i < harf.Length; i++)
+                {
+                   if (unsuz.Contains(harf[i]) && unsuz.Contains(harf[i + 1]));
+                   {
+                        kontrol = true;
+                        break;
+                   } 
+                }
+                Console.Write(" "+ kontrol);
+            }
+
+        }
+    }
+}
